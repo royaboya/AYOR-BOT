2f18accdfe7d4583b0c7693682fe746a5ccb3698
royaboya <94084201+royaboya@users.noreply.github.com>
2022-03-06 18:42:16 -0500

    a moving thing

--- cogs/Management.py
+++ cogs/Management.py
@@ -9,5 +9,11 @@ class Management(commands.Cog):
     async def ping(self, ctx, member:discord.Member):
         await ctx.send(member.mention, delete_after=10)
 
+    @commands.command(aliases=["move"])
+    async def move_all(self, ctx, current : discord.VoiceChannel=None, destination : discord.VoiceChannel=None):
+        for member_id in current.voice_states.keys():
+            memb = ctx.guild.get_member(member_id)
+            await memb.move_to(destination)
+
 def setup(client):
     client.add_cog(Management(client))
