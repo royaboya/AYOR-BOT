f4b98fa62cf97fed822e19849d5903de926ecd82
royaboya <94084201+royaboya@users.noreply.github.com>
2022-03-06 15:38:55 -0500

    old code

---
+++ main.py
@@ -0,0 +1,31 @@
+# testing without cogs first
+
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = '' # idc lol
+
+intents = discord.Intents.default()
+intents.members = True
+client = commands.Bot(command_prefix='~', intents=intents)
+
+
+# class Help(commands.MinimalHelpCommand):
+#     async def send_pages(self):
+#         destination = self.get_destination()
+#         for page in self.paginator.pages:
+#             emby = discord.Embed(description=page)
+#             await destination.send(embed=emby)
+
+# client.help_command = Help()
+
+for file in os.listdir('./cogs'):
+    if file.endswith('.py'):
+        client.load_extension(f'cogs.{file[:-3]}')
+
+if __name__ == '__main__':
+    client.run(TOKEN)
+
+
+
